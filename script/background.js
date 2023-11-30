8ddb610e45bb98fc11329063b80fd28f4a293cdb
cuihaicheng <cuihaicheng@aggrx.com>
2023-11-30 16:53:38 +0800

    feature:读取剪贴板

--- script/background.js
+++ script/background.js
@@ -5,11 +5,11 @@ chrome.contextMenus.create({
 })
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
-	// background.js
-
-	// 发送消息到内容脚本
+  
 	chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-	  chrome.tabs.sendMessage(tabs[0].id, { message: "Hello from background!" });
+	  chrome.tabs.sendMessage(tabs[0].id, { message: "clicked menu" });
 	});
 
 })
+
+
--- script/test.js
+++ script/test.js
@@ -2,12 +2,11 @@
 	console.log("hello1");
 })();
 
-function doConvert() {
+function doConvert(code) {
 	const textArea = document.getElementById("prompt-textarea");
 	const sendButton = document.querySelector('[data-testid="send-button"]');
 	if (textArea) {
-		textArea.value = "hello";
-		console.log("find");
+		textArea.value = code;
 		
 		const event = new Event('input', {bubbles: true})
 		textArea.dispatchEvent(event)
@@ -23,6 +22,18 @@ function doConvert() {
 // 接收来自后台脚本的消息
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
   console.log("Received message from background:", message);
-  doConvert()
+  
+  
+  navigator.clipboard.readText()
+    .then(text => {
+      console.log('剪贴板内容：', text);
+      // 在这里可以处理读取到的文本内容
+    })
+    .catch(err => {
+      console.error('无法读取剪贴板内容：', err);
+    });
+
+
+  // doConvert(message.message)
 });
 
